b29c27430271dd975541f51310061f9b33243089
shunpin tseng <shunpin@gmail.com>
2020-09-20 13:43:48 -0700

    updatesWithSaveBtnClick

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Javascrip-DayPlanner
+/# Javascrip-DayPlanner
+/
+## This exercise is utilizing javascript to dynamically generate time slots from 9am to 6pm for workday planning.
+
+. HTML is left empty while using javascirpt to create and inser elemetns to HTML. Use browser to view the layout of the page.
+. css is provided and all styles and effects are called from javascrip for interactive function.
+. One issue remain to be resolved is the clicked hover effect won't exacuate and seem to disable the lahyout.
+
+by Shunpin Tseng 
--- script.js
+++ script.js
@@ -25,6 +25,8 @@
 //feeding time and date information from moment.js and place date info to html
 var date
 var hour
+var timeArr = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM", "6PM"]
+
 $.ajax({
   url: "https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js",
   method: "GET"
@@ -38,60 +40,17 @@ $.ajax({
 
 // creat time table for the table layout with each row by hour using JQ to assign each html elements
 
-var timeArr = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM", "6PM"]
 var t;
 for (t = 0; t < timeArr.length; t++) {
   console.log(timeArr[t]);
-  
+
   var row = $('<section class="row time-block"></section>');
   var timeSlot = $('<time class="col-md-1 hour"></time>');
-  var textArea = $('<textarea class="col-md-10"></textarea>');
-  var button = $('<buton class="col-md-1 saveBtn">SAVE</buton>')
-  
-
-
-
-
-
-
-
-
-  var textAreaName = "#textarea"+timeArr[t]
-
-  var buttonObj = {
-
-
-    textAreaNameinternal : "hola",
-    foo(){alert(this)
-    localStorage.setItem(this.textAreaNameinternal, $(this.textAreaNameinternal).text)
-    
-  }
-  
-}
-
-buttonObj.foo()
-
-
-
-//buttonObj.textAreaNameinternal = textAreaName;
-button.click(buttonObj.foo);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  // $("button").hover(function(){
-  //   $(this).css("saveBtn i:hover")
-  //   });
+  var textAreaID = "textarea" + t;
+  var textArea = $('<textarea class="col-md-10" id ="' + textAreaID + '"></textarea>');
+  var buttonID = "button" + t
+  var button = $('<buton class="col-md-1 saveBtn" id ="' + buttonID + '" >SAVE</buton>')
+  button.on("click", saveBtnClick)
 
   row.append(timeSlot);
   row.append(textArea);
@@ -102,40 +61,39 @@ button.click(buttonObj.foo);
   console.log($(".container").append(row));
 }
 
-var saveButton = 
-
 //setting up an var for fixedTime minus 9 to corresponding to array index no. of calendar time slot
 //for each if statement the index [i] will be compared to the fixedTime to determine if its past, present or future.
-function colorTimeSlot(timeArr, date){
-  var fixedTime = parseInt(date)-9;
-for (var i = 0; i<timeArr.length; i++){
-  timeArr[i].children[1].classList.remove("past");
-  timeArr[i].children[1].classList.remove("future");
-  timeArr[i].children[1].classList.remove("present");
-  
-  //remove all pre-assigned css class for color setting "past","future","present"
-  if(i==fixedTime){
-  timeArr[i].children[1].classList.add("present");
-  }
-  if(i<fixedTime){
-  timeArr[i].children[1].classList.add("past");
-  }
-  if(i>fixedTime){
-    timeArr[i].children[1].classList.add("future");
+function colorTimeSlot (timeArr, date) {
+  var fixedTime = parseInt(date) - 9;
+  for (var i = 0; i < timeArr.length; i++) {
+    timeArr[i].children[1].classList.remove("past");
+    timeArr[i].children[1].classList.remove("future");
+    timeArr[i].children[1].classList.remove("present");
+
+    //remove all pre-assigned css class for color setting "past","future","present"
+    if (i == fixedTime) {
+      timeArr[i].children[1].classList.add("present");
     }
+    if (i < fixedTime) {
+      timeArr[i].children[1].classList.add("past");
+    }
+    if (i > fixedTime) {
+      timeArr[i].children[1].classList.add("future");
+    }
+  }
 }
+
+//save text area message to local storage. create and assign index ID for each textarea so that when save button is clicked. The information entered will be sent to local storage
+
+function saveBtnClick(){
+
+var textAreaNo = this.id.replace("button","textarea"); 
+
+var textAreaElement = document.getElementById(textAreaNo);
+
+
+localStorage.setItem(textAreaNo, textAreaElement.value)
+
 }
-
-
-// // To be used code
-// localStorage.setItem("test", "My first note")
-// licalStorage.setItem("test1", "My second note")
-
-// var txFromStorage = localStorage.getItem("test")
-// $(".first").text(txFromStorage)
-
-// console.log(moment().format("h A")
-
-
 
 
